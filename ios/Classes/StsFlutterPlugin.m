dca54cf683306f37e8101fd37a0679dc75edfafc
AHECA <ahca_developer@163.com>
2020-10-19 16:51:09 +0800

    修改插件类的换行符以及格式化代码

--- ios/Classes/StsFlutterPlugin.m
+++ ios/Classes/StsFlutterPlugin.m
@@ -10,151 +10,273 @@
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-    if ([@"init" isEqualToString:call.method]) {
+    if ([@"init" isEqualToString:call.method])
+    {
         NSString * baseUrl = call.arguments[@"baseUrl"];
         NSString * stsAppKey = call.arguments[@"stsAppKey"];
         NSString * stsSecretKey = call.arguments[@"stsSecretKey"];
         result(@"");
-    } else if ([@"initUseId" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"initUseId" isEqualToString:call.method])
+    {
         NSString * useId = call.arguments[@"useId"];
         result(@"");
-    } else if ([@"initFace" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"initFace" isEqualToString:call.method])
+    {
         NSString * fileId = call.arguments[@"id"];
         NSString * fileName = call.arguments[@"fileName"];
         result(@"");
-    } else if ([@"initThemeColor" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"initThemeColor" isEqualToString:call.method])
+    {
         NSNumber * color = call.arguments[@"color"];
         result(@"");
-    } else if ([@"getDeviceID" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"getDeviceID" isEqualToString:call.method])
+    {
         result(@"");// NSString
-    } else if ([@"isLocalCertExist" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"isLocalCertExist" isEqualToString:call.method])
+    {
         result(@"");// BOOL
-    } else if ([@"checkCert" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"checkCert" isEqualToString:call.method])
+    {
         result(@"");// CommonResult => JsonString {"resultCode": int, "resultMsg": NSString}
-    } else if ([@"clearCert" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"clearCert" isEqualToString:call.method])
+    {
         result(@"");// CommonResult => JsonString {"resultCode": int, "resultMsg": NSString}
-    } else if ([@"applyPersonalCert" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"applyPersonalCert" isEqualToString:call.method])
+    {
         NSString * stsUserInfo = call.arguments[@"stsUserInfo"];
         // JsonString => StsUserInfo
         result(@"");// ApplyCertResult => JsonString
-    } else if ([@"applyCompanyCert" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"applyCompanyCert" isEqualToString:call.method])
+    {
         NSString * stsCompanyInfo = call.arguments[@"stsCompanyInfo"];
         // JsonString => StsCompanyInfo
         result(@"");// ApplyCertResult => JsonString
-    } else if ([@"getUntieEquipmentQRCode" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"getUntieEquipmentQRCode" isEqualToString:call.method])
+    {
         result(@"");// GetQRCodeResult => JsonString
-    } else if ([@"untieEquipment" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"untieEquipment" isEqualToString:call.method])
+    {
         NSString * qrData = call.arguments[@"qrData"];
         result(@"");// ApplyCertResult => JsonString
-    } else if ([@"updatePersonalCert" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"updatePersonalCert" isEqualToString:call.method])
+    {
         NSString * stsUserInfo = call.arguments[@"stsUserInfo"];
         // JsonString => StsUserInfo
         result(@"");// CommonResult => JsonString
-    } else if ([@"updateCompanyCert" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"updateCompanyCert" isEqualToString:call.method])
+    {
         NSString * stsCompanyInfo = call.arguments[@"stsCompanyInfo"];
         // JsonString => StsCompanyInfo
         result(@"");// CommonResult => JsonString
-    } else if ([@"resetPersonalPIN" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"resetPersonalPIN" isEqualToString:call.method])
+    {
         NSString * stsUserInfo = call.arguments[@"stsUserInfo"];
         // JsonString => StsUserInfo
         result(@"");// CommonResult => JsonString
-    }  else if ([@"resetCompanyPIN" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"resetCompanyPIN" isEqualToString:call.method])
+    {
         NSString * stsCompanyInfo = call.arguments[@"stsCompanyInfo"];
         // JsonString => StsCompanyInfo
         result(@"");// CommonResult => JsonString
-    } else if ([@"modifyPIN" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"modifyPIN" isEqualToString:call.method])
+    {
         result(@"");// CommonResult => JsonString
-    } else if ([@"scanLogin" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"scanLogin" isEqualToString:call.method])
+    {
         NSString * stsScanInfo = call.arguments[@"stsScanInfo"];
         // JsonString => StsScanInfo
         result(@"");// CommonResult => JsonString
-    } else if ([@"scanSign" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"scanSign" isEqualToString:call.method])
+    {
         NSString * stsScanInfo = call.arguments[@"stsScanInfo"];
         // JsonString => StsScanInfo
         result(@"");// CommonResult => JsonString
-    } else if ([@"certLogin" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"certLogin" isEqualToString:call.method])
+    {
         NSString * data = call.arguments[@"data"];
         NSString * dataFormat = call.arguments[@"dataFormat"];
         NSString * dataType = call.arguments[@"dataType"];
         NSString * pn = call.arguments[@"pn"];
         result(@"");// CertLoginResult => JsonString
-    } else if ([@"certSeal" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"certSeal" isEqualToString:call.method])
+    {
         NSString * pn = call.arguments[@"pn"];
         result(@"");// CertSealResult => JsonString
-    } else if ([@"certSign" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"certSign" isEqualToString:call.method])
+    {
         NSString * data = call.arguments[@"data"];
         NSString * dataFormat = call.arguments[@"dataFormat"];
         NSString * dataType = call.arguments[@"dataType"];
         NSString * pn = call.arguments[@"pn"];
         result(@"");// CertSignResult => JsonString
-    } else if ([@"certVerifySign" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"certVerifySign" isEqualToString:call.method])
+    {
         NSString * data = call.arguments[@"data"];
         NSString * dataFormat = call.arguments[@"dataFormat"];
         NSString * signData = call.arguments[@"signData"];
         NSString * dataType = call.arguments[@"dataType"];
         result(@"");// CommonResult => JsonString
-    } else if ([@"certEncrypt" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"certEncrypt" isEqualToString:call.method])
+    {
         NSString * data = call.arguments[@"data"];
         NSString * dataFormat = call.arguments[@"dataFormat"];
         NSString * dataType = call.arguments[@"dataType"];
         result(@"");// CertEncryptResult => JsonString
-    } else if ([@"certDecrypt" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"certDecrypt" isEqualToString:call.method])
+    {
         NSString * data = call.arguments[@"data"];
         NSString * dataFormat = call.arguments[@"dataFormat"];
         NSString * dataType = call.arguments[@"dataType"];
         NSString * pn = call.arguments[@"pn"];
         result(@"");// CertDecryptResult => JsonString
-    } else if ([@"changeCertStatus" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"changeCertStatus" isEqualToString:call.method])
+    {
         NSNumber * statusTypeN = call.arguments[@"statusType"];
         int statusType = [statusTypeN intValue];
         result(@"");// CommonResult => JsonString
-    } else if ([@"postponeCert" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"postponeCert" isEqualToString:call.method])
+    {
         result(@"");// CommonResult => JsonString
-    } else if ([@"setPKCacheTime" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"setPKCacheTime" isEqualToString:call.method])
+    {
         NSString * pn = call.arguments[@"pn"];
         result(@"");// PKCacheResult => JsonString
-    } else if ([@"clearPKCacheTime" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"clearPKCacheTime" isEqualToString:call.method])
+    {
         NSString * pn = call.arguments[@"pn"];
         result(@"");// PKCacheResult => JsonString
-    } else if ([@"getCert" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"getCert" isEqualToString:call.method])
+    {
         NSNumber * certTypeN = call.arguments[@"certType"];
         int certType = [certTypeN intValue];
         result(@"");// GetCertResult => JsonString
-    } else if ([@"getLocalCert" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"getLocalCert" isEqualToString:call.method])
+    {
         NSNumber * certTypeN = call.arguments[@"certType"];
         int certType = [certTypeN intValue];
         result(@"");// GetCertResult => JsonString
-    } else if ([@"downloadCert" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"downloadCert" isEqualToString:call.method])
+    {
         NSString * phoneNum = call.arguments[@"phoneNum"];
         NSString * departmentNo = call.arguments[@"departmentNo"];
         NSNumber * certTypeN = call.arguments[@"certType"];
         int certType = [certTypeN intValue];
         result(@"");// GetCertResult => JsonString
-    } else if ([@"getDepartmentNo" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"getDepartmentNo" isEqualToString:call.method])
+    {
         result(@"");// GetDepartmentNoResult => JsonString
-    } else if ([@"setSignImgWithDrawingBoard" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"setSignImgWithDrawingBoard" isEqualToString:call.method])
+    {
         NSString * stsSignImgSetting = call.arguments[@"stsSignImgSetting"];// NullAble
         result(@"");// SignImgResult => JsonString
-    } else if ([@"setSignImgBase64Str" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"setSignImgBase64Str" isEqualToString:call.method])
+    {
         NSString * imgBase64 = call.arguments[@"imgBase64"];
         result(@"");// SignImgResult => JsonString
-    } else if ([@"getSignImgFromService" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"getSignImgFromService" isEqualToString:call.method])
+    {
         result(@"");// SignImgResult => JsonString
-    } else if ([@"getSignImgAndSetItIfNotExist" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"getSignImgAndSetItIfNotExist" isEqualToString:call.method])
+    {
         NSString * stsSignImgSetting = call.arguments[@"stsSignImgSetting"];// NullAble
         result(@"");// SignImgResult => JsonString
-    } else if ([@"getSignImgWithDrawingBoard" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"getSignImgWithDrawingBoard" isEqualToString:call.method])
+    {
         NSString * stsSignImgSetting = call.arguments[@"stsSignImgSetting"];// NullAble
         result(@"");// SignImgResult => JsonString
-    } else if ([@"getFingerprintStatus" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"getFingerprintStatus" isEqualToString:call.method])
+    {
         result(@"");// Bool
-    } else if ([@"openFingerprint" isEqualToString:call.method]) {
+    }
+    
+    else if ([@"openFingerprint" isEqualToString:call.method])
+    {
         NSNumber * openN = call.arguments[@"open"];
         BOOL open = [openN boolValue];
         result(@"");// CommonResult => JsonString
-    } else {
+    }
+    
+    else
+    {
         result(FlutterMethodNotImplemented);
-    }
+    }
+    
 }
 
 @end
